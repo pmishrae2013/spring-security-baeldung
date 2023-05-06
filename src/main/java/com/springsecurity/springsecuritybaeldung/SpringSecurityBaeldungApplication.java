8f04e6e32a62df78c808d05db6ccefe12a27470f
purushottam <pmishrae2013@gmail.com>
2023-05-06 20:58:31 +0530

    initial commit

---
+++ src/main/java/com/springsecurity/springsecuritybaeldung/SpringSecurityBaeldungApplication.java
@@ -0,0 +1,13 @@
+package com.springsecurity.springsecuritybaeldung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityBaeldungApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityBaeldungApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springsecurity/springsecuritybaeldung/SpringSecurityBaeldungApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springsecurity.springsecuritybaeldung;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityBaeldungApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
